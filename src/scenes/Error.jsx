a7659df095c369c29ec52901c791578bc77b4d28
Alex Kim <lawofambak7@gmail.com>
2023-02-27 13:49:35 -0800

    Update Error page style

--- src/scenes/Error.jsx
+++ src/scenes/Error.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
-// Margin is 0 in html page so need to fix this somehow
-const errorstyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-};
-
 const Error = () => {
   const error = useRouteError();
   console.error(error);
   
   return (
-    <div style={errorstyle}>
+    <div className="error">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
