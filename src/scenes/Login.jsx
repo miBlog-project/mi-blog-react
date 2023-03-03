4c5e7bd01013a45be4c03e187c4f6966e8ca069e
Alex Kim <lawofambak7@gmail.com>
2023-03-02 21:16:24 -0800

    Work on user authentication

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
+  const [userInputs, setUserInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleInputChange = e => {
+    setUserInputs(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+      })
+    );
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    
+    try {
+      await axios.post("/auth/login", userInputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  }
+
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="Username" />
-        <input required type="password" placeholder="Password" />
-        <button>Login</button>
-        <p>Error Message goes here</p>
+        <input required type="text" placeholder="Username" name="username" onChange={handleInputChange} />
+        <input required type="password" placeholder="Password" name="password" onChange={handleInputChange} />
+        <button onClick={handleSubmit}>Login</button>
+        {error && <p>{error}</p>}
         <span>No account? <Link to="/register">Register here</Link>
         </span>
       </form>
--- src/scenes/Register.jsx
+++ src/scenes/Register.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+  const [userInputs, setUserInputs] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleInputChange = e => {
+    setUserInputs(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+      })
+    );
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    
+    try {
+      await axios.post("/auth/register", userInputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  }
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="email" placeholder="Email" />
-        <input required type="text" placeholder="Username" />
-        <input required type="password" placeholder="Password" />
-        <button>Register</button>
-        <p>Error Message goes here</p>
+        <input required type="email" placeholder="Email" name="email" onChange={handleInputChange} />
+        <input required type="text" placeholder="Username" name="username" onChange={handleInputChange} />
+        <input required type="password" placeholder="Password" name="password" onChange={handleInputChange} />
+        <button onClick={handleSubmit}>Register</button>
+        {error && <p>{error}</p>}
         <span>Already have account? <Link to="/login">Login here</Link>
         </span>
       </form>
